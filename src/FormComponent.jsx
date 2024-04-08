f8a7366bb1d24eb8e06cd5db5cd1fd07450bc015
jenny-berggreen <jenny@berggreen.no>
2024-04-08 14:24:14 +0200

    Style

--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,55 +1,56 @@
 import React from 'react';
 import styles from './FormComponent.module.css'
+import appStyles from './App.module.css'
 
 const FormComponent = () => {
 	return (
 		<>
-		<form className='contact-form'>
-			<fieldset>
+		<form className={styles.contact_form}>
+			<fieldset className={styles.contact_form_container}>
 				<legend>Contact us</legend>
 
-				<section>
-					<div>
+				<section className={styles.name_section}>
+					<div className={styles.input_group}>
 						<label htmlFor="firstName">First name<sup>*</sup></label>
-						<input type="text" name='firstName' placeholder='Enter your first name' />
+						<input type="text" name='firstName' placeholder='Enter your first name' className={styles.input_element} />
 						<p>First name error</p>
 					</div>
-					<div>
+					<div className={styles.input_group}>
 						<label htmlFor="lastName">Last name<sup>*</sup></label>
-						<input type="text" name='lastName' placeholder='Enter your last name' />
+						<input type="text" name='lastName' placeholder='Enter your last name' className={styles.input_element} />
 						<p>Last name error</p>
 					</div>
 				</section>
 
-				<section>
-					<div>
+				<section className={styles.contact_section}>
+					<div className={styles.input_group}>
 						<label htmlFor="email">E-mail<sup>*</sup></label>
-						<input type="email" name='email' placeholder='Enter your e-mail' />
+						<input type="email" name='email' placeholder='Enter your e-mail' className={styles.input_element} />
 						<p>E-mail error</p>
 					</div>
-					<div>
+					<div className={styles.input_group}>
 						<label htmlFor="phoneNumber">Phone number<sup>*</sup></label>
-						<input type="text" name='phoneNumber' placeholder='Enter your phone number' />
+						<input type="text" name='phoneNumber' placeholder='Enter your phone number' className={styles.input_element} />
 						<p>Phone number error</p>
 					</div>
 				</section>
 
-				<div>
+				<div className={styles.input_group}>
 					<label htmlFor="subject">Subject<sup>*</sup></label>
-					<input type="subject" name='subject' placeholder='Enter the subject (max 20 characters)' />
+					<input type="subject" name='subject' placeholder='Enter the subject (max 20 characters)' className={styles.input_element} />
 					<p>Subject error</p>
 				</div>
 
-				<div>
+				<div className={styles.input_group}>
 					<label htmlFor="message">Message<sup>*</sup></label>
-					<textarea name="message" id="" cols="30" rows="10" placeholder='Enter your message (max 300 characters)'></textarea>
+					<textarea name="message" cols="30" rows="10" placeholder='Enter your message (max 300 characters)' className={styles.textarea_element}></textarea>
 					<div>
 						<p>Message error</p>
 						<p>Message count</p>
 					</div>
 				</div>
 
-				<button>Submit</button>
+				<button className={styles.submit_button}>Submit</button>
 			</fieldset>
 		</form>
 		</>
